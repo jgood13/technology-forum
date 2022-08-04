540d6ab008202cfdb7a06b1473c209d94151ba89
jgood13 <joseph.good8@gmail.com>
2022-08-04 17:23:43 -0600

    added helper for date, added some basic routes

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,12 @@
 const seedPosts = require("./post-seeds");
-const seedUsers = require("./user-seeds");
+const seedUser = require("./user-seeds");
 const seedComments = require("./comment-seeds");
 
 const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  await seedUsers();
+  await seedUser();
   await seedPosts();
   await seedComments();
 
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine(
   "handlebars",
